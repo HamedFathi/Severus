a6e52077a33e059473940c58c2ba3d78238f552b
HamedFathi <noeffect@outlook.com>
2020-07-25 11:55:41 +0430

    chore(update csproj)

--- ParserSample/Program.cs
+++ ParserSample/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 
 namespace ParserSample
-{     
+{
     class Program
     {
         static void Main(string[] args)
         {
-            var query = "  SELECT C1,C2,C3 FROM T1 WHERE C1=5.23 AN C2> 2.6 OR C3<2  ";
+            var query = "  SELECT C1,C2,C3 FROM T1 WHERE C1=5.23 AND C2> 2.6 OR C3<2  ";
             var lexer = new SqlLexer(query).Tokenize();
             var parser = new SqlParser(lexer).Parse();
-            Console.WriteLine("Hello World!");
         }
     }
 }
